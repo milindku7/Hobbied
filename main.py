84051b4736ea4cce351cb015c12408702cbd9f0b
Milind Kumar <143648132+milindku7@users.noreply.github.com>
2024-06-27 17:27:34 -0500

    added login and verified signup

--- main.py
+++ main.py
@@ -51,10 +51,14 @@ def validate_password(password):
             return True
     return False
 
-def validate_username(username):
+def escape_characters(username):
     username = username.replace("&","&amp;")
     username = username.replace("<","&lt;")
     username = username.replace(">","&gt;")
+    return username
+
+def validate_username(username):
+    username = escape_characters(username)
     user_found = users.find_one({"username": username})
     if user_found is None:
         return True
@@ -63,7 +67,17 @@ def validate_username(username):
 
 @app.route("/")
 def serve_index():
-    return render_template("login.html")
+    auth_token = request.cookies.get("auth_token")
+    auth_token = auth_token.encode("utf-8")
+    auth_token = hashlib.sha256(auth_token)
+    auth_token = auth_token.hexdigest()
+    found_auth_token = users.find_one({"authtoken": auth_token})
+    if found_auth_token:
+        pass
+    
+    
+        
+    return render_template("index.html")
 
 @app.route("/signup")
 def serve_signup():
@@ -109,6 +123,44 @@ def after_signup():
 @app.route("/login")
 def login_page():
     return render_template("login.html")
+
+@app.route("/login/login_details")
+def check_login():
+    req_json = request.get_json()
+    username = req_json["username"]
+    username = helper.auth.extract_credentials(username)
+    username = escape_characters(username)
+    found_user = users.find_one({"username": username})
+    if found_user:
+        received_pass = req_json["password"]
+        received_pass = helper.auth.extract_credentials(received_pass)
+        received_pass = helper.auth.modify_pass(received_pass)
+        og_pass = found_user["password"]
+        if og_pass == received_pass:
+            if found_user["authtoken"] == "":
+                new_token = uuid.uuid4()
+                new_token = str(new_token)
+                new_token = new_token.encode("utf-8")
+                new_token = hashlib.sha256(new_token).hexdigest()
+                users.update_one({"username": username},{"$set" : {"authtoken": new_token}})
+            if found_user["XSRF"] == "":
+                new_xsrf = uuid.uuid4()
+                new_xsrf = str(new_xsrf)
+                users.update_one({"username": username},{"$set" : {"XSRF": new_xsrf}})
+            found_user = users.find_one({"username": username})
+            fin_auth = found_user["authtoken"]
+            data = {
+            "message": "notright"
+            }
+            response = make_response(jsonify(data))
+            response.set_cookie("auth_token",fin_auth,max_age=31536000,httponly=True,secure=True)
+            response.headers['X-Content-Type-Options'] = 'nosniff'
+            return response
+    else:
+        data = {
+            "message": "notright"
+        }
+        return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True,port=8080)
--- static/js/functions.js
+++ static/js/functions.js
@@ -37,4 +37,29 @@ async function signup_form_submit() {
             document.getElementById("error_message").innerHTML = "This email is not valid";
         }
 }
+async function login_process() {
+    const username = document.getElementById("username").value.toLowerCase();
+    const password = document.getElementById("password").value;
+    const JSON_message = {
+        "username": encodeURIComponent(username),
+        "password": encodeURIComponent(password)
+    };
 
+    const URL_curr = window.location.href;
+    const concated = URL_curr.concat("/login_details");
+    const res = await fetch(concated,{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(JSON_message)
+    });
+    const data = await res.json();
+    const message = data.message;
+    if (message === "notright")
+        {
+            document.getElementById("error_message").innerHTML = "Please check the username and password again";
+        }
+    if (message === "allright")
+        {
+            window.location.href = "/";
+        }
+}
